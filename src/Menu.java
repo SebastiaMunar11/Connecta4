385bd2212aafbc078a3db82d8e70fb13d47a68f3
Sebastia-Munar-Alcover <smunaralcover11@gmail.com>
2022-04-13 10:12:18 +0200

    demanarAccio

--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,6 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Menu {
     private Taulell taulell;
     private String torn; //"yellow" o "red"
@@ -16,7 +19,42 @@ public class Menu {
         System.out.println("\n\n<<--CONNECTA 4-->>");
     }
 
+    public void demanarAccio() {
+        System.out.println("Trïa una columna:");
+        int columna;
 
+        try{
+            Scanner sc = new Scanner(System.in);
+            columna= sc.nextInt();
+
+            if(!taulell.isColumnaPlena()){
+                if(torn.equals("red")){
+                    Fitxa fitxa = new Fitxa("red");
+                    taulell.posarFitxa(fitxa, columna);
+                }
+                if(torn.equals("yellow")){
+                    Fitxa fitxa = new Fitxa("yellow");
+                    taulell.posarFitxa(fitxa, columna);
+                }
+                if(torn.equals("red")){
+                    torn= "yellow";
+                }
+                if(torn.equals("yellow")){
+                    torn= "red";
+                }
+            }
+            else{
+                System.out.println("La columna està plena");
+            }
+
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Columna inexistent");
+        }
+        catch (InputMismatchException e){
+            System.out.println("Informació Invàlida");
+        }
+    }
 
 
 }

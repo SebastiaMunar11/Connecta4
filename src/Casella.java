aa67184f4567289ada55850a4435944a4c4e5664
Sebastia-Munar-Alcover <smunaralcover11@gmail.com>
2022-04-13 09:03:53 +0200

    creat les classes

--- src/Casella.java
+++ src/Casella.java
@@ -1,5 +1,24 @@
 public class Casella {
 
-    private
+    private boolean isPlena = false;
+    private String fitxa; // "red" "yellow"
 
+    public boolean isPlena() {
+        return isPlena;
+    }
+
+    public String getFitxa() {
+        return fitxa;
+    }
+
+    private void setPlena(boolean plena) {
+        isPlena = plena;
+    }
+
+    public void setFitxa(String fitxa) {
+        if(fitxa.equals("red") || fitxa.equals("yellow")){
+            this.fitxa = fitxa;
+            setPlena(true);
+        }
+    }
 }
--- src/Taulell.java
+++ src/Taulell.java
@@ -1,2 +1,17 @@
 public class Taulell {
+
+    private Casella[][] caselles = new Casella[6][7];
+
+    private void omplirCaselles() {
+        for (int i = 0; i < caselles.length; i++) {
+            for (int j = 0; j < caselles[0].length; j++) {
+                caselles[i][j] = new Casella();
+            }
+        }
+    }
+
+
+
+
+
 }

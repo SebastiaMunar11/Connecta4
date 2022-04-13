f96cc8de5bfe429bb4ed437ba6f24e084e8437d3
Sebastia-Munar-Alcover <smunaralcover11@gmail.com>
2022-04-13 09:17:46 +0200

    displayTaulell

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        Taulell taulell = new Taulell();
+        taulell.getCaselles()[5][0].setFitxa("red");
+        taulell.getCaselles()[5][1].setFitxa("red");
+        taulell.getCaselles()[5][2].setFitxa("red");
+        taulell.getCaselles()[5][3].setFitxa("yellow");
+        taulell.displayTaulell();
     }
 }
--- src/Taulell.java
+++ src/Taulell.java
@@ -2,6 +2,14 @@ public class Taulell {
 
     private Casella[][] caselles = new Casella[6][7];
 
+    public Taulell(){
+        omplirCaselles();
+    }
+
+    public Casella[][] getCaselles() {
+        return caselles;
+    }
+
     private void omplirCaselles() {
         for (int i = 0; i < caselles.length; i++) {
             for (int j = 0; j < caselles[0].length; j++) {
@@ -10,7 +18,45 @@ public class Taulell {
         }
     }
 
+    public void displayTaulell() {
+        int fila = 0;
+        int columna = 0;
 
+        System.out.print("     ");
+        for (columna = 0; columna < caselles[0].length; columna++) {
+            if (columna < 10) {
+                System.out.print("  " + columna + "  ");
+            } else {
+                System.out.print("  " + columna + "  ");
+            }
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < caselles.length; i++) {
+            if (fila < 10) {
+                System.out.print("  " + fila + "  ");
+            } else {
+                System.out.print("  " + fila + " ");
+            }
+            fila++;
+            for (int j = 0; j < caselles[0].length; j++) {
+                if (caselles[i][j].isPlena()) {
+                    if(caselles[i][j].getFitxa().equals("red")){
+                        System.out.print("[ R ]");
+                    }
+                    if(caselles[i][j].getFitxa().equals("yellow")){
+                        System.out.print("[ Y ]");
+                    }
+                }else {
+                    System.out.print("[   ]");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+    }
 
 
 
